7f7ed69fb92ab098f34b876970b41d17f668d271
StrahinjaLat <latinovicstrahinja0@gmail.com>
2025-02-18 00:24:56 +0700

    Button hover animation

--- moj-projekat/src/main.js
+++ moj-projekat/src/main.js
@@ -146,9 +146,83 @@ function closeSuccessMessage() {
              
           });
   });
-  // Dodavanje event listenera za dugme "Zatvori"
+//   // Dodavanje event listenera za dugme "Zatvori"
 document.getElementById('closeButton').addEventListener('click', closeSuccessMessage);
 });
+
+
+
+const buttonss = document.querySelector('.footer__submit-button');
+
+
+  const filler = document.querySelector('.footer__button-filler');
+  const textInner = document.querySelector('.footer__button-text-inner');
+
+  buttonss.addEventListener('mouseenter', () => {
+    // Filler animation
+    
+    buttonss.classList.add('button--hover');
+    // Dodavanje klase active na body
+    document.body.classList.add('active');
+    
+
+
+   const tl = gsap.timeline()
+    tl.to(filler, {
+      duration: 0.5,
+      startAt: { y: '75%' }, // Početna pozicija na ulazu
+      y: '0%',
+      ease: 'Power3.easeOut'
+    });
+
+    // Text animation
+    tl.to(textInner, {
+      duration: 0.1,
+      opacity: 0,
+      startAt: { y: '30%', opacity: 1 },
+      y: '-10%',
+      ease: 'Power3.easeOut'
+    }, 0);
+
+    tl.to(textInner, {
+      duration: 0.25,
+      opacity: 1,
+      y: '0%',
+      ease: 'Power3.easeOut',
+      delay: 0.1
+    }, 0,1);
+  });
+
+  buttonss.addEventListener('mouseleave', () => {
+
+    buttonss.classList.remove('button--hover');
+    // Uklanjanje klase active sa body
+    document.body.classList.remove('active');
+    const tl = gsap.timeline()
+    // Filler animation reverse
+    tl.to(filler, {
+      duration: 0.4,
+      y: '-75%',
+      ease: 'Power3.easeOut'
+    });
+
+    // Text animation reverse
+    tl.to(textInner, {
+      duration: 0.1,
+      opacity: 0,
+      y: '10%',
+      ease: 'Power3.easeOut'
+    }, 0);
+
+    tl.to(textInner, {
+      duration: 0.25,
+      opacity: 1,
+      startAt: { y: '-30%', opacity: 1 }, 
+      y: '0%',
+      ease: 'Power3.easeOut',
+      delay: 0.1
+    }, 0.1);
+  });
 
 
 
